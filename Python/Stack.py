670039aaa04f5fa414b6f87375e389f7454fa4ca
Ben3940 <bty2015@comcast.net>
2024-02-01 19:30:25 -0500

    Defined methods for Stack class.  Need to implement unit tests next

--- Python/Stack.py
+++ Python/Stack.py
@@ -4,13 +4,30 @@ from Node import Node
 class Stack:
 
     def __init__(self):
-        pass
+        self.length = 0
+        self.tail = None
 
     def push(self, value):
-        pass
+        node = Node(value)
+        self.length += 1
+        if not self.tail:
+            self.tail = node
+        else:
+            node.set_next_node(self.tail)
+            self.tail = node
 
     def pop(self):
-        pass
+        if not self.tail:
+            return
+
+        self.length -= 1
+        value = self.tail.get_value()
+        self.tail = self.tail.get_next()
+
+        if self.length <= 0:
+            self.tail = None
+        return value
 
     def peek(self):
-        pass
+        if self.tail:
+            return self.tail.get_value()
