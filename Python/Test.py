05bf367a6194febdb80e0fbcd1f6b161050efb78
Ben3940 <bty2015@comcast.net>
2024-02-01 19:38:18 -0500

    Added unit tests for Stack class under Python

--- Python/Test.py
+++ Python/Test.py
@@ -1,5 +1,6 @@
 import unittest
 from Node import Node
+from Stack import Stack
 
 
 class TestNode(unittest.TestCase):
@@ -17,6 +18,32 @@ class TestNode(unittest.TestCase):
         node = Node(0)
         self.node.set_next_node(node)
         self.assertEqual(self.node.get_next(), node)
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.pop(), 1)
+
+    def test_pop(self):
+        self.assertEqual(self.stack.pop(), None)
+
+        self.stack.push(3)
+        self.stack.push(4)
+        self.stack.push(5)
+        self.assertEqual(self.stack.pop(), 5)
+        self.stack.pop()
+        self.assertEqual(self.stack.pop(), 3)
+
+    def test_peek(self):
+        self.assertEqual(self.stack.peek(), None)
+        self.stack.push(6)
+        self.assertEqual(self.stack.peek(), 6)
 
 
 if __name__ == "__main__":
