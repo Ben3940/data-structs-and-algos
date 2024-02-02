a20c2ae8067e226f75668e820012a2032cc837c9
Ben3940 <bty2015@comcast.net>
2024-02-01 19:22:30 -0500

    Created Stack class with method signatures under Python

---
+++ Python/Stack.py
@@ -0,0 +1,16 @@
+from Node import Node
+
+
+class Stack:
+
+    def __init__(self):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
