7539e49814f515028814728197df11c0f056f633
Ben3940 <bty2015@comcast.net>
2024-02-02 20:48:25 -0500

    Created Queue class with method signatures under Python

---
+++ Python/Queue.py
@@ -0,0 +1,16 @@
+from Node import Node
+
+
+class Queue:
+
+    def __init__(self):
+        pass
+
+    def enqueue(self, value):
+        pass
+
+    def deque(self):
+        pass
+
+    def peek(self):
+        pass
