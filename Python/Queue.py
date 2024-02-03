445a69fe4e5289d327c07691b729c432058b734d
Ben3940 <bty2015@comcast.net>
2024-02-02 20:51:49 -0500

    Defined methods for Queue class under Python

--- Python/Queue.py
+++ Python/Queue.py
@@ -4,13 +4,33 @@ from Node import Node
 class Queue:
 
     def __init__(self):
-        pass
+        self.length = 0
+        self.head = self.tail = None
 
     def enqueue(self, value):
-        pass
+        self.length += 1
+        node = Node(value)
+
+        if not self.tail:
+            self.head = self.tail = node
+
+        else:
+            self.tail.set_next_node(node)
+            self.tail = node
 
     def deque(self):
-        pass
+        if not self.head:
+            return None
+
+        self.length -= 1
+        val = self.head.get_value()
+        self.head = self.head.get_next()
+
+        if self.length == 0:
+            self.tail = None
+
+        return val
 
     def peek(self):
-        pass
+        if self.head:
+            return self.head.get_value()
