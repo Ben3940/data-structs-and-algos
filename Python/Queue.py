852dd120055c01de424e1c0f0e1da7c9559a39c9
Ben3940 <bty2015@comcast.net>
2024-02-02 21:03:46 -0500

    Added test cases for Queue class under Python and commented code in Queue.py

--- Python/Queue.py
+++ Python/Queue.py
@@ -8,24 +8,32 @@ class Queue:
         self.head = self.tail = None
 
     def enqueue(self, value):
+        # Increment Queue length for new node
         self.length += 1
         node = Node(value)
 
+        # If no tail, make head and tail point to new node
         if not self.tail:
             self.head = self.tail = node
 
         else:
+            # Update next node to new node before setting the new node as tail
             self.tail.set_next_node(node)
             self.tail = node
 
     def deque(self):
+        # If no head then nothing is in Queue
         if not self.head:
             return None
 
+        # Decrement Queue length and get current head node's value
         self.length -= 1
         val = self.head.get_value()
+
+        # Update head to point to next node in Queue
         self.head = self.head.get_next()
 
+        # Need to set tail to None so that in enqueue() head and tail will be set again
         if self.length == 0:
             self.tail = None
 
