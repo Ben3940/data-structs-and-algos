fd98c1f106fa6440297419f0eddfb0ff4db74894
Ben3940 <bty2015@comcast.net>
2024-02-05 18:30:32 -0500

    Created LinkedList class with method signatures

---
+++ Python/LinkedList.py
@@ -0,0 +1,22 @@
+from Nodes import Node
+
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        pass
+
+    def get(self, idx):
+        pass
+
+    def set(self, idx, value):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def size(self):
+        pass
