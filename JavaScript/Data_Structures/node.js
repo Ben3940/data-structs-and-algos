5c3a7205d310304f5715fe9065f8d31fc5b65541
Ben3940 <bty2015@comcast.net>
2024-02-12 10:43:37 -0500

    Corrected naming convention issue in node.js

--- JavaScript/Data_Structures/node.js
+++ JavaScript/Data_Structures/node.js
@@ -4,19 +4,19 @@ class Node {
     this.next_node = null;
   }
 
-  get_value() {
+  getValue() {
     return this.value;
   }
 
-  get_next_node() {
+  getNextNode() {
     return this.next_node;
   }
 
-  set_value(value) {
+  setValue(value) {
     this.value = value;
   }
 
-  set_next_node(node) {
+  setNextNode(node) {
     this.next_node = node;
   }
 }
